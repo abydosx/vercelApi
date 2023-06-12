8383be269512dc8124e496fbed3730793b72c95a
xieyulong <569837500@qq.com>
2023-06-12 20:25:40 +0800

    feat: api response format

--- routes/notion.js
+++ routes/notion.js
@@ -5,29 +5,27 @@ var router = express.Router();
 router.post('/pages', function (req, res, next) {
   const params = req.body;
   query(params).then((results) => {
-    res.json(results);
+    res.json({ ...results, status_code: 200 });
   }).catch((err) => {
-    res.json(err);
+    res.json({ ...err, status_code: 400 });
   })
 });
 
 router.post('/updateDatabase', function (req, res, next) {
   const params = req.body;
   updateDatabase(params).then(results => {
-    res.json(results);
+    res.json({ ...results, status_code: 200 });
   }).catch((err) => {
-    console.log('error')
-    res.json(err);
+    res.json({ ...err, status_code: 400 });
   })
 });
 
 router.post('/updatePage', function (req, res, next) {
   const params = req.body;
   updatePage(params).then(results => {
-    res.json(results);
+    res.json({ ...results, status_code: 200 });
   }).catch((err) => {
-    console.log('error')
-    res.json(err);
+    res.json({ ...err, status_code: 400 });
   })
 });
 
